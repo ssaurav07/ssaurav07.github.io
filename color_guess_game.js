54cb99056bd8a4a7f0415c89e4725198e031ea39
ssquare143 <62537472+ssquare143@users.noreply.github.com>
2020-07-17 10:54:27 +0530

    game_almost_complete

--- color_guess_game.js
+++ color_guess_game.js
@@ -1,6 +1,7 @@
 var easy = document.getElementById("easy_span");
 var hard = document.getElementById("hard_span");
 
+/*
 easy.addEventListener("mouseover",function(){
     this.style.backgroundColor="blueviolet";
     this.style.color="white";   
@@ -20,6 +21,21 @@ hard.addEventListener("mouseout",function(){
     this.style.backgroundColor="white";
     this.style.color="blueviolet";   
 });
+*/
+
+easy.addEventListener("click",function(){
+    this.style.backgroundColor="blueviolet";
+    this.style.color="white";
+    hard.style.backgroundColor="white";
+    hard.style.color="blueviolet";
+});
+
+hard.addEventListener("click",function(){
+    this.style.backgroundColor="blueviolet";
+    this.style.color="white";
+    easy.style.backgroundColor="white";
+    easy.style.color="blueviolet";
+});
 
 function genRandomColor()
 {
@@ -29,29 +45,61 @@ function genRandomColor()
 
 var rgb_heading=document.querySelector(".heading");
 
+var colum=document.querySelectorAll(".column");
+
 var colum_length = document.querySelectorAll(".column").length;
 
 var newColor=document.getElementById("new_color_span");
 
-    newColor.addEventListener("click",function(){
+    newColor.addEventListener("click",color_change);
+                              
+function color_change(){
+       
+    document.getElementById("new_color_span").textContent="NEW COLORS";
+    document.getElementById("tryAgain").textContent="";
+    document.querySelector(".head_div").style.backgroundColor="blueviolet";
+        var random_number=(Math.floor(Math.random()*6));
+        
         for(var i=0;i<(colum_length);i++){
-                var colum=document.getElementsByClassName("column")[i];
-                colum.style.backgroundColor=genRandomColor();
-            
-                var random_number=(Math.floor(Math.random()*7));
+                colum[i].style.backgroundColor=genRandomColor();
+
                 if(i===Number(random_number))
                     {
-                        rgb_heading.textContent=(colum.style.backgroundColor);
+                        window.match_color=(colum[i].style.backgroundColor);
+                        rgb_heading.textContent=match_color;
                     }
-            };
-        ;
+            }
         
+        }
+
+color_change();
+
+for(var i=0;i<(colum_length);i++){
+    
+        colum[i].addEventListener("click",function(){
+            
+            var clicked_color=this.style.backgroundColor;
+            
+            if(clicked_color== match_color)
+                {
+                    alert("RIGHT ! RIGHT  ! RIGHT !");
+                    
+                    for(var i=0;i<(colum_length);i++){
+                    colum[i].style.backgroundColor=match_color;
+                    }
+                    
+                    document.querySelector(".head_div").style.backgroundColor=match_color;
+                    
+                    document.getElementById("tryAgain").textContent="Well Done!";
+                    
+                    document.getElementById("new_color_span").textContent="PLAY AGAIN?";
+                    
+                }
+            
+            else{
+                alert("WRONG ! WRONG ! WRONG !");
+                document.getElementById("tryAgain").textContent="Try Again!";
+                this.style.backgroundColor="rgb(4,24,0)";
+            }
         });
-
-
-
-
-
-
-
-
+    }
