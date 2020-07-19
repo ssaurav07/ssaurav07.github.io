7ebd9dab4b3b4246e8b6b650d4c39c80d85c87e3
ssquare143 <62537472+ssquare143@users.noreply.github.com>
2020-07-19 11:06:26 +0530

    toDoList

--- toDoList.js
+++ toDoList.js
@@ -1,8 +1,34 @@
-$(".fa-trash").on("click",function(event){
-    $(this).parent().parent().fadeOut(function(){
+
+
+$("#heading span").on("click",function(){
+    $(".fa-plus").toggle();
+    $("input").toggle();
+    
+})
+
+$("input").keypress(function(event){
+    if(event.which === 13){
+            $("ul").append('<li><span><i class="fa fa-trash"></i></span>'+$(this).val()+'</li>');
+            $("input").toggle();
+            $(".fa-plus").toggle();
+            $(this).val("");
+            
+        
+        }
+    });
+
+$("li span").on("click",function(event){
+    $(this).parent().fadeOut(function(){
         $(this).remove();
     });
     
     event.stopPropagation();
     
 }) 
+
+$("li").on("click",function(){
+    $(this).css({
+        "text-decoration":"line-through",
+        "text-decoration-color":"red"
+    });
+})
