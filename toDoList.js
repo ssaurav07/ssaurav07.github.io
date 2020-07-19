a55c17707fd3a21da76f3f7decdceadf0f4b4db8
ssquare143 <62537472+ssquare143@users.noreply.github.com>
2020-07-19 11:35:36 +0530

    g

--- toDoList.js
+++ toDoList.js
@@ -1,23 +1,4 @@
-
-
-$("#heading span").on("click",function(){
-    $(".fa-plus").toggle();
-    $("input").toggle();
-    
-})
-
-$("input").keypress(function(event){
-    if(event.which === 13){
-            $("ul").append('<li><span><i class="fa fa-trash"></i></span>'+$(this).val()+'</li>');
-            $("input").toggle();
-            $(".fa-plus").toggle();
-            $(this).val("");
-            
-        
-        }
-    });
-
-$("li span").on("click",function(event){
+$("ul").on("click","span",function(event){
     $(this).parent().fadeOut(function(){
         $(this).remove();
     });
@@ -26,9 +7,26 @@ $("li span").on("click",function(event){
     
 }) 
 
-$("li").on("click",function(){
+$("ul").on("click","li",function(){
     $(this).css({
         "text-decoration":"line-through",
         "text-decoration-color":"red"
     });
 })
+
+$("#heading span").on("click",function(){
+    $(".fa-plus").toggle();
+    $("input").toggle();
+    
+})
+
+$("input").on("keypress",function(event){
+    if(event.which === 13){
+            var newtodo= $(this).val();
+            $(this).val("");
+            $("ul").append("<li><span><i class='fa fa-trash'></i></span>"+newtodo+"</li>");
+            $("input").toggle();
+            $(".fa-plus").toggle();
+            
+        }
+    });
